f768874665c128c6a1b988bd79e0262149f54b46
CaioMatInt <caiointre@gmail.com>
2024-06-03 20:13:06 -0300

    Fixed route

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,8 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    //@@TODO: Should check how to change the home path
+    Route::get('dashboard', \App\Livewire\Boards::class)->middleware('auth')->name('dashboard');
+    Route::get('boards', \App\Livewire\Boards::class)->middleware('auth');
+    Route::get('boards/{id}', \App\Livewire\ShowBoards::class)->middleware('auth')->name('boards.show');
 });
-
-Route::get('boards', \App\Livewire\Boards::class)->middleware('auth');
-Route::get('boards/{id}', \App\Livewire\ShowBoards::class)->middleware('auth')->name('boards.show');
