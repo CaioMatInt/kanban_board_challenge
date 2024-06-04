aa51118e481dd2389fd6885b6c0f34cbe6d15e9c
CaioMatInt <caiointre@gmail.com>
2024-06-03 22:22:32 -0300

    Added missing constrained on foreign keys

--- database/migrations/2024_06_02_021755_create_board_lists_table.php
+++ database/migrations/2024_06_02_021755_create_board_lists_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('order');
-            $table->foreignIdFor(Board::class);
+            $table->foreignIdFor(Board::class)->constrained();
             $table->timestamps();
         });
     }
--- database/migrations/2024_06_02_021755_create_list_tasks_table.php
+++ database/migrations/2024_06_02_021755_create_list_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->integer('order');
-            $table->foreignIdFor(BoardList::class);
+            $table->foreignIdFor(BoardList::class)->constrained();
             $table->timestamps();
         });
     }
